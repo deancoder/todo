cf645d58ad45c33b9d1833572bc0e48f638ede51
deancoder <rk1308@yahoo.com>
2015-02-09 07:50:48 +0530

    updated

--- dailytask/views.py
+++ dailytask/views.py
@@ -23,7 +23,6 @@ def index(request):
 		return render_to_response("index.html", context_instance=RequestContext(request))
 	elif request.method == "POST":
 		email = request.POST.get("email")
-                print "email 1", email
 		if User.objects.filter(email = email).exists():
 			return render_to_response("home.html",{'email': email }, context_instance=RequestContext(request))
 		else:
@@ -39,7 +38,6 @@ def home(request):
 		return render_to_response("home.html",{'email': email }, context_instance=RequestContext(request))
 	elif request.method == "POST":
 		email = request.POST.get("email")
-                print "email 1", email
 		if User.objects.filter(email = email).exists():
 			todolist = TodayTask.objects.filter(email = email)
 			return render_to_response("workdairy.html",{'todos': todolist, 'email': email }, context_instance=RequestContext(request))
@@ -55,13 +53,13 @@ def dairy(request):
 	if request.method == "GET":
 		email = request.user
 		todolist = TodayTask.objects.filter(email = email, status = 'OPEN')
-		print 'email', email
+		
 		return render_to_response("workdairy.html",{'todos': todolist, 'email': email }, context_instance=RequestContext(request))
 	elif request.is_ajax():
 		email = request.POST.get("email")
 		wid = request.POST.get("wid")
 		action = request.POST.get("action")
-		print "wid",
+		
 		if TodayTask.objects.filter(pk=wid).exists():
 			task = TodayTask.objects.get(pk=wid)
 			if(action == 'completed'):
@@ -73,16 +71,16 @@ def dairy(request):
 		return HttpResponse(json.dumps(content), mimetype='application/json')
 	elif request.method == "POST":
 		email = request.POST.get("email")
-		print "dairy", email
+		
 		name = request.POST.get("wname")
-		print "dairy",name
+		
 		timein = request.POST.get("start_time")
 		time = datetime.datetime.strptime(timein, "%H:%M")
 		creation_date = date.today()
 		task = TodayTask.objects.create(email = email, date = creation_date, task_name = name, task_time = time)
 		messages.success(request, "Task is added to todo list ")
 		todolist = TodayTask.objects.filter(email = email, status = 'OPEN')
-		print "todo", todolist
+		
 		return render_to_response("workdairy.html",{'todos': todolist, 'email': email }, context_instance=RequestContext(request))
 	else:
 		return render_to_response("index.html", context_instance=RequestContext(request))
@@ -102,6 +100,6 @@ def profile(request):
 			user.name = name
 			user.phone = phone
 		userinfo = User.objects.filter(email = email)
-		print userinfo
+		
 		return render_to_response("profile.html", {'user': userinfo, 'email': email }, context_instance=RequestContext(request))
 
